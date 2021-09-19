53765407cb2099773dacd601f9ae6a98713481e3
banksjazz87 <banksjazz87@gmail.com>
2021-09-19 07:17:37 -0400

    chore: I am working on my menu display feature, I just need to change some styling

--- js/index.js
+++ js/index.js
@@ -1,4 +1,51 @@
-let i = 0;
+//A variable to keep track of the current height of the menu_list display
+let x = 0;
+
+//Function to create an effect for the menu items to dynamically appear.
+const showMenu = () => {
+
+    clearInterval(widthHeight());
+    x = 0;
+    let menuList = document.getElementById('menu_items');
+
+    if (menuList.style.display === "none") {
+        return () => {
+            widthHeight();
+            menuList.style.display = "flex";
+        }
+
+    } else {
+        return menuList.style.display = "none";
+    }
+}
+
+//This function will be to create the width and height for the menu_items
+
+const widthHeight = () => {
+    let menuList = document.getElementById('menu_items');
+
+    setInterval(() => {
+        if (x < 100) {
+            x++;
+            console.log(x);
+            return menuList.style = "height:" + x + "vh; width:" + x + "vw;";
+        } else {
+            console.log(x);
+        }
+    }, );
+}
+
+
+
+const menu = document.getElementById('menu_container');
+
+menu.addEventListener('click', () => {
+    //widthHeight();
+    showMenu();
+});
+
+
+/*let i = 0;
 
 const testing = () => {
     setInterval(() => {
@@ -15,4 +62,4 @@ const testing = () => {
 //clearInterval(intTest);
 
 
-testing();
+testing();*/
